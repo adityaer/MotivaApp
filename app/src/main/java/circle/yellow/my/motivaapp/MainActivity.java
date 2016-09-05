0a8795e0f9d0eb453e362ba84e84c5d8fea15d1b
aditya <aditya.eka.r@gmail.com>
2016-09-05 17:09:54 +0700

    Test Git 2

--- app/src/main/java/circle/yellow/my/motivaapp/MainActivity.java
+++ app/src/main/java/circle/yellow/my/motivaapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.v("Test","Testosteron");
+        Log.v("Test 2","Test 2");
     }
 }
